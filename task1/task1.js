088f5b1ca1a94741be8126f90f18302fc0989d26
Jyoti379 <haniyajo45@gmail.com>
2022-08-01 23:12:56 +0530

    index

---
+++ task1/task1.js
@@ -0,0 +1,45 @@
+//alert(1);
+
+
+
+//console.dir(document);
+
+//console.log(document.domain);
+
+//console.log(document.URL);
+
+//console.log(document.title);
+
+//console.log(document.doctype);
+
+/*console.log(document.head);
+
+console.log(document.body);
+
+console.log(document.all[10]);
+
+document.all[10].textContent ='Hello';
+
+console.log(document.forms);
+
+console.log(document.links);
+
+console.log(document.images);
+
+
+
+console.log(document.getElementById('header-title'))
+
+var headerTitle =document.getElementById('header-title');
+
+console.log(headerTitle);
+
+headerTitle.textContent ='Hello';
+
+headerTitle.innerText='Goodbye';//diiference between inner text and text content is one of them pays attention to styling
+
+//adding style to text content shows the text inside the style also,but inner text pays attention to style and donot diplays text iside style
+
+headerTitle.innerHTML ='<h3>Hello</h3>';
+
+headerTitle.style.borderBottom ='solid 3px #000';*/
